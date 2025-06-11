a0e7c9b2e4a27c67e00886f3a9718f1064a0db0c
nindmork <stnindmork@gmail.com>
2025-06-11 16:06:11 +0700

    update crispip

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+function scrollToAbout(event) {
+  event.preventDefault();
+
+  // ดึง href ที่อยู่ในลิงก์ เช่น "#about"
+  const targetId = event.currentTarget.getAttribute("href");
+  const element = document.querySelector(targetId);
+
+  if (element) {
+    const value = 2;
+    const yOffset = -window.innerHeight / value + element.offsetHeight / value;
+    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+}
